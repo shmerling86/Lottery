257090c2b6e97ac45b6ba7b184c17f4fe5ecf75a
shmerling86 <shmerling86@gmail.com>
2018-11-08 12:16:53 +0200

    new colors

--- list/listCtrl.js
+++ list/listCtrl.js
@@ -10,218 +10,219 @@ app.controller('listCtrl', function ($scope, listSrv, loginSrv, $location, $log)
 
     $scope.logout = function () {
         loginSrv.logout();
-        $location.path('/');
+        $location.path('/login');
     }
 
-    $scope.custom = true;
-
-    $scope.userId = loginSrv.getActiveUser().id;
+  
 
 
-    listSrv.getTheName($scope.userId).then(function (name) {
-        $scope.name = name
+
+
+$scope.custom = true;
+
+$scope.userId = loginSrv.getActiveUser().id;
+
+
+listSrv.getTheName($scope.userId).then(function (name) {
+    $scope.name = name
+});
+
+$scope.toggleCustom = function () {
+    $scope.participation = 0;
+
+    $scope.custom = $scope.custom === false ? true : false;
+
+    if ($scope.custom == false) {
+        listSrv.getJoinDate($scope.userId).then(function (userDataParticipation) {
+
+
+            if (userDataParticipation == undefined) {
+                $scope.custom = true;
+
+                return
+            } else {
+                $scope.participations = userDataParticipation
+
+                for (var i = 0; i < $scope.participations.length; i++) {
+                    moment.locale('he');
+                    $scope.participations[i]['time'] = moment($scope.participations[i]['time']).fromNow()
+                }
+            }
+
+        }, function (error) {
+            $log.error(error)
+        });
+
+        listSrv.getTheWinner($scope.userId).then(function (userDataWinner) {
+
+            if (userDataWinner == undefined) {
+                return
+            } else {
+
+                $scope.winners = userDataWinner
+
+                for (var i = 0; i < $scope.winners.length; i++) {
+                    moment.locale('he');
+                    $scope.winners[i]['time'] = moment($scope.winners[i]['time']).fromNow()
+                }
+            }
+
+
+        }, function (error) {
+            $log.error(error)
+        });
+
+        listSrv.getTheLoser($scope.userId).then(function (userDataLoser) {
+
+            if (userDataLoser == undefined) {
+                return
+            } else {
+
+                $scope.losers = userDataLoser
+
+                for (var i = 0; i < $scope.losers.length; i++) {
+                    moment.locale('he');
+                    $scope.losers[i]['time'] = moment($scope.losers[i]['time']).fromNow()
+                }
+            }
+
+
+        }, function (error) {
+            $log.error(error)
+        });
+
+    }
+
+};
+
+$scope.kindOfSort = "active";
+
+$scope.sortByStatus = function (item) {
+
+    switch ($scope.kindOfSort) {
+        case "all":
+            return true;
+        case "active":
+            if (item.complete < 100) return true;
+            break;
+
+        case "completed":
+            if (item.complete >= 100) return true;
+            break;
+    }
+    return false;
+}
+
+// $scope.lotteries = [];
+$scope.competitors = [];
+
+listSrv.getAllLotteries().then(function (lotteries) {
+
+    $scope.lotteries = lotteries;
+
+    for (var i = 0; i < lotteries.length; i++) {
+        moment.locale('he');
+        lotteries[i]['startTime'] = moment(lotteries[i]['startTime']).fromNow()
+    }
+
+}, function (error) {
+    $log.error(error)
+});
+
+$scope.completePercentage = 0;
+
+$scope.isAlreadyIn = function (idx) {
+
+    $scope.alreadyIn = false;
+    listSrv.getAllCompetitors(idx).then(function (competitors) {
+        for (let i = 0; i < competitors.length; i++) {
+            if ($scope.userId == competitors[i]) {
+                $scope.alreadyIn = true;
+            }
+        }
+    }, function (error) {
+        $log.error(error)
     });
 
-    $scope.toggleCustom = function () {
-        $scope.participation = 0;
+}
 
-        $scope.custom = $scope.custom === false ? true : false;
+$scope.getAndCount = function (btn, index) {
 
-        if ($scope.custom == false) {
-            listSrv.getJoinDate($scope.userId).then(function (userDataParticipation) {
-
-
-                if (userDataParticipation == undefined) {
-                    $scope.custom = true;
-
-                    return
-                } else {
-                    $scope.participations = userDataParticipation
-
-                    for (var i = 0; i < $scope.participations.length; i++) {
-                        moment.locale('he');
-                        $scope.participations[i]['time'] = moment($scope.participations[i]['time']).fromNow()
-                    }
-                }
-
-            }, function (error) {
-                $log.error(error)
-            });
-
-            listSrv.getTheWinner($scope.userId).then(function (userDataWinner) {
-
-                if (userDataWinner == undefined) {
-                    return
-                } else {
-
-                    $scope.winners = userDataWinner
-
-                    for (var i = 0; i < $scope.winners.length; i++) {
-                        moment.locale('he');
-                        $scope.winners[i]['time'] = moment($scope.winners[i]['time']).fromNow()
-                    }
-                }
+    $scope.custom = true;
+    moment.locale('he');
+    $scope.clickTime = moment().format();
 
 
-            }, function (error) {
-                $log.error(error)
-            });
+    listSrv.getJoinDate($scope.userId).then(function (userDataParticipation) {
+        $scope.participations = userDataParticipation
 
-            listSrv.getTheLoser($scope.userId).then(function (userDataLoser) {
+        listSrv.dateTheJoin($scope.clickTime, $scope.lotteries[index]['productName'], $scope.participations).then(function (participation) {
 
-                if (userDataLoser == undefined) {
-                    return
-                } else {
+            $scope.participation = participation.length
 
-                    $scope.losers = userDataLoser
-
-                    for (var i = 0; i < $scope.losers.length; i++) {
-                        moment.locale('he');
-                        $scope.losers[i]['time'] = moment($scope.losers[i]['time']).fromNow()
-                    }
-                }
-
-
-            }, function (error) {
-                $log.error(error)
-            });
-
-        }
-
-    };
-
-    $scope.kindOfSort = "active";
-
-    $scope.sortByStatus = function (item) {
-
-        switch ($scope.kindOfSort) {
-            case "all":
-                return true;
-            case "active":
-                if (item.complete < 100) return true;
-                break;
-
-            case "completed":
-                if (item.complete >= 100) return true;
-                break;
-        }
-        return false;
-    }
-
-    // $scope.lotteries = [];
-    $scope.competitors = [];
-
-    listSrv.getAllLotteries().then(function (lotteries) {
-
-        $scope.lotteries = lotteries;
-
-        for (var i = 0; i < lotteries.length; i++) {
-            moment.locale('he');
-            lotteries[i]['startTime'] = moment(lotteries[i]['startTime']).fromNow()
-        }
+        }, function (error) {
+            $log.error(error)
+        });
 
     }, function (error) {
         $log.error(error)
     });
 
-    $scope.completePercentage = 0;
 
-    $scope.isAlreadyIn = function (idx) {
+    $scope.competitors.push($scope.lotteries[index]);
 
-        $scope.alreadyIn = false;
-        listSrv.getAllCompetitors(idx).then(function (competitors) {
-            for (let i = 0; i < competitors.length; i++) {
-                if ($scope.userId == competitors[i]) {
-                    $scope.alreadyIn = true;
-                }
-            }
-        }, function (error) {
-            $log.error(error)
-        });
+    listSrv.getAllLotteries().then(function (lotteries) {
 
-    }
-
-    $scope.getAndCount = function (btn, index) {
-
-        $scope.custom = true;
-        moment.locale('he');
-        $scope.clickTime = moment().format();
+        var lengOfCompetittors = lotteries[index].competitors.length
+        var numOfParticipants = lotteries[index].numberOfParticipants
+        var howMuchFinish = lotteries[index].complete
 
 
-        listSrv.getJoinDate($scope.userId).then(function (userDataParticipation) {
-            $scope.participations = userDataParticipation
+        if (lengOfCompetittors == 0) {
 
-            listSrv.dateTheJoin($scope.clickTime, $scope.lotteries[index]['productName'], $scope.participations).then(function (participation) {
+            $scope.completePercentage = ((1 / numOfParticipants) * 100)
 
-                $scope.participation = participation.length
+        } else if (lengOfCompetittors > 0 && lengOfCompetittors < numOfParticipants) {
 
-            }, function (error) {
-                $log.error(error)
-            });
+            $scope.completePercentage = ((lengOfCompetittors + 1) / numOfParticipants) * 100;
 
-        }, function (error) {
-            $log.error(error)
-        });
+        } else if (howMuchFinish >= 100) {
+            $scope.completePercentage = 100
+        }
 
-
-        $scope.competitors.push($scope.lotteries[index]);
-
-        listSrv.getAllLotteries().then(function (lotteries) {
-
-            var lengOfCompetittors = lotteries[index].competitors.length
-            var numOfParticipants = lotteries[index].numberOfParticipants
-            var howMuchFinish = lotteries[index].complete
-
-
-            if (lengOfCompetittors == 0) {
-
-                $scope.completePercentage = ((1 / numOfParticipants) * 100)
-
-            } else if (lengOfCompetittors > 0 && lengOfCompetittors < numOfParticipants) {
-
-                $scope.completePercentage = ((lengOfCompetittors + 1) / numOfParticipants) * 100;
-
-            } else if (howMuchFinish >= 100) {
-                $scope.completePercentage = 100
-            }
-
-            // for (var i = 0; i < lotteries.length; i++) {
-            //     moment.locale('he');
-            //     lotteries[i]['startTime'] = moment(lotteries[i]['startTime']).fromNow()
-            // }
+        // for (var i = 0; i < lotteries.length; i++) {
+        //     moment.locale('he');
+        //     lotteries[i]['startTime'] = moment(lotteries[i]['startTime']).fromNow()
+        // }
 
 
 
-            listSrv.getAllCompetitors(index).then(function (competitors) {
+        listSrv.getAllCompetitors(index).then(function (competitors) {
 
-                $scope.competitorsId = competitors
+            $scope.competitorsId = competitors
 
-                listSrv.countMeIn(index, $scope.competitorsId, $scope.completePercentage).then(function () {
+            listSrv.countMeIn(index, $scope.competitorsId, $scope.completePercentage).then(function () {
 
-                    btn.target.disabled = true;
+                btn.target.disabled = true;
 
-                    if ($scope.completePercentage == 100) {
+                if ($scope.completePercentage == 100) {
 
-                        listSrv.lotteryGen(index).then(function (winnerId) {
-                            $scope.idOfWinner = winnerId
-                            moment.locale('he');
-                            $scope.time = moment().format();
+                    listSrv.lotteryGen(index).then(function (winnerId) {
+                        $scope.idOfWinner = winnerId
+                        moment.locale('he');
+                        $scope.time = moment().format();
 
-                            listSrv.patchTheWinner($scope.idOfWinner, lotteries[index]['productName'], $scope.time).then(function () {
-                                $scope.lossers = [];
-                                for (let i = 0; i < $scope.competitorsId.length; i++) {
-                                    if ($scope.competitorsId[i] != $scope.idOfWinner) {
-                                        $scope.lossers.push($scope.competitorsId[i])
-                                    }
+                        listSrv.patchTheWinner($scope.idOfWinner, lotteries[index]['productName'], $scope.time).then(function () {
+                            $scope.lossers = [];
+                            for (let i = 0; i < $scope.competitorsId.length; i++) {
+                                if ($scope.competitorsId[i] != $scope.idOfWinner) {
+                                    $scope.lossers.push($scope.competitorsId[i])
                                 }
+                            }
 
-                                listSrv.patchTheLoser(lotteries[index]['productName'], $scope.time, $scope.lossers).then(function () {
+                            listSrv.patchTheLoser(lotteries[index]['productName'], $scope.time, $scope.lossers).then(function () {
 
-                                    
 
-                                }, function (error) {
-                                    $log.error(error)
-                                });
 
                             }, function (error) {
                                 $log.error(error)
@@ -230,11 +231,11 @@ app.controller('listCtrl', function ($scope, listSrv, loginSrv, $location, $log)
                         }, function (error) {
                             $log.error(error)
                         });
-                    }
 
-                }, function (error) {
-                    $log.error(error)
-                });
+                    }, function (error) {
+                        $log.error(error)
+                    });
+                }
 
             }, function (error) {
                 $log.error(error)
@@ -244,9 +245,13 @@ app.controller('listCtrl', function ($scope, listSrv, loginSrv, $location, $log)
             $log.error(error)
         });
 
+    }, function (error) {
+        $log.error(error)
+    });
 
 
-    };
+
+};
 
 
 });
