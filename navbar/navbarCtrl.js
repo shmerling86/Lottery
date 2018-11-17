00e94792dd993c401c6fafa2979506416119bfd2
shmerling86 <shmerling86@gmail.com>
2018-11-17 18:47:50 +0200

    sass change

--- navbar/navbarCtrl.js
+++ navbar/navbarCtrl.js
@@ -2,6 +2,9 @@ app.controller('navbarCtrl', function ($scope, loginSrv, listSrv, $location) {
 
     if (loginSrv.isLoggedIn()) {
         $location.path('/list');
+    } else{
+        $location.path('/login');
+
     }
 
     $scope.logout = function () {
